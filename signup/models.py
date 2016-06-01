e1acd94b6ff03be1054dd7144d743616b063cbce
aaditi <aaditidhadwal@gmail.com>
2016-06-01 13:46:16 +0530

    user model update

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django import forms
+
+
+class signup(models.Model):
+    username = models.TextField(max_length=200)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+    confirmpassword = forms.CharField(
+        max_length=32, widget=forms.PasswordInput)
 
 # Create your models here.
--- signup/views.py
+++ signup/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
+def post_list(request):
+    return render(request, 'signup/register.html', {})
+
 # Create your views here.
