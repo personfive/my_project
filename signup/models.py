4cc58ac23fdbc26cfa63ec5ee18c5a74fccf30a6
aaditi <aaditidhadwal@gmail.com>
2016-06-01 13:49:17 +0530

    user model updated

--- signup/models.py
+++ signup/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django import forms
 
 
-class signup(models.Model):
+class user(models.Model):
     username = models.TextField(max_length=200)
     password = forms.CharField(max_length=32, widget=forms.PasswordInput)
-    confirmpassword = forms.CharField(
-        max_length=32, widget=forms.PasswordInput)
+    confirmpassword = forms.CharField(max_length=32, widget=forms.PasswordInput)
 
 # Create your models here.
