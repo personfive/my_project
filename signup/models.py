d5cd6966805917abafa21406a5d2a45d6dda3101
aaditi <aaditidhadwal@gmail.com>
2016-06-01 13:53:06 +0530

    usermodel updated

--- signup/models.py
+++ signup/models.py
@@ -5,6 +5,5 @@ from django import forms
 class user(models.Model):
     username = models.TextField(max_length=200)
     password = forms.CharField(max_length=32, widget=forms.PasswordInput)
-    confirmpassword = forms.CharField(max_length=32, widget=forms.PasswordInput)
 
 # Create your models here.
